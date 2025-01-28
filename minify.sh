d6d773a4d125326b6232ca0f091e5da0919d091b
sagiadinos <ns@smil-control.com>
2025-01-28 16:11:03 +0100

    update minify.sh

--- minify.sh
+++ minify.sh
@@ -4,3 +4,4 @@ export PATH=$PATH:$(npm bin -g)
 terser src/SpicyBase.js --compress --mangle --output dist/SpicyBase.min.js
 terser src/SpicyCam.js --compress --mangle --output dist/SpicyCam.min.js
 terser src/SpicyCast.js --compress --mangle --output dist/SpicyCast.min.js
+
