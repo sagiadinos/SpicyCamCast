87d05745d3c0df6e7f4605780970108699d592c8
sagiadinos <ns@smil-control.com>
2025-01-29 11:21:28 +0100

    edit examples

--- minify.sh
+++ minify.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export PATH=$PATH:$(npm bin -g)
-terser src/SpicyBase.js --compress --mangle --output dist/SpicyBase.min.js
-terser src/SpicyCam.js --compress --mangle --output dist/SpicyCam.min.js
-terser src/SpicyCast.js --compress --mangle --output dist/SpicyCast.min.js
-
+export PATH=$PATH:$(npm bin -g) # required for macos
+cat src/SpicyBase.js src/SpicyCam.js  src/SpicyCast.js > dist/SpicyCamCast1.js
+sed 's/import {SpicyBase} from "\.\/SpicyBase\.js";//g' dist/SpicyCamCast1.js > dist/SpicyCamCast.js
+unlink dist/SpicyCamCast1.js
+terser dist/SpicyCamCast.js --compress --mangle --output dist/SpicyCamCast.min.js
+unlink dist/SpicyCamCast.js
